ab93be928ebc6285fd7f4578aec5659372dc21a0
maulik <maulikp60@gmail.com>
2025-03-14 10:02:45 +1100

    added comment

--- app/src/main/java/com/example/cryptocurrency/App.kt
+++ app/src/main/java/com/example/cryptocurrency/App.kt
@@ -13,6 +13,7 @@ class App : Application() {
     companion object {
         private var instance: App? = null
 
+        // return context which is accessible throughout the application
         fun appContext(): Context {
             return instance!!.applicationContext
         }
